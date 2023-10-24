66a622adba306ee33a2f636f8f4a86bee84afaa0
Ivan David ALFONSO DIAZ <ivandaalf@hotmail.com>
2023-10-24 18:27:44 +0200

    Transformation of end associations to class attributes

--- src/generators/python_classes/templates/python_classes_template.py
+++ src/generators/python_classes/templates/python_classes_template.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 {% for class in classes %}
 class {{ class.name }}{% if class.parents() %}({% for parent in class.parents() %}{{parent.name}}{{ ", " if not loop.last else "" }}{% endfor %}){% endif %}:
-    def __init__(self{% for attribute in class.all_attributes() %}, {{ attribute.name }}:{{ attribute.type.name }}{% endfor %}):
+    def __init__(self{% for attribute in class.all_attributes() %}, {{ attribute.name }}: {{ attribute.type.name }}{% endfor %}
+                 {%- for end in class.association_ends() if end.is_navigable == True %}, {{ end.name }}: {% if end.multiplicity.max > 1 %}set[{{ end.type.name }}]
+                 {%- else %}{{ end.type.name }}{%- endif %}{%- endfor %}):
         {% if class.inherited_attributes() %}super().__init__({% for attr in class.inherited_attributes() %}{{attr.name}}{{ ", " if not loop.last else "" }}{% endfor %})
         {% endif -%}
         {% for attribute in class.attributes %}self.{{ attribute.name }} = {{ attribute.name }}

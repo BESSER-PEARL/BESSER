454afaa075c5a33da27f85126f4984ed0213b521
Ivan David ALFONSO DIAZ <ivandaalf@hotmail.com>
2023-11-07 16:06:04 +0100

    serialization of any model (object)

--- src/BUML/utilities/utilities.py
+++ src/BUML/utilities/utilities.py
@@ -5,10 +5,14 @@ class ModelSerializer():
     def __init__(self) -> None:
         pass
 
-    def dump(self, model: DomainModel, output_dir: str=None):
-        file_path = model.name + ".buml"
+    def dump(self, model: any, output_dir: str=None, output_file_name: str = None):
+        file_path = output_file_name
+        if output_file_name == None:
+            file_path = "model.pkl"
+            if type(model) == DomainModel:
+                file_path = model.name + ".buml"
         if output_dir != None:
-            file_path = os.path.join(output_dir, model.name)
+            file_path = os.path.join(output_dir, file_path)
         with open(file_path, "wb") as f:
             pickle.dump(model, f)
             f.close()

bd2c6c6bd93f2298dd1f5167c3f1b965aa7b8fce
ivan-alfonso <ivandaalf@hotmail.com>
2023-11-24 12:44:33 +0100

    Use build_generation_path method

--- src/generators/django/django_generator.py
+++ src/generators/django/django_generator.py
@@ -8,15 +8,9 @@ class DjangoGenerator(GeneratorInterface):
         super().__init__(model, output_dir)
 
     def generate(self):
-        file_name = "models.py"
-        working_path = os.path.abspath('')
+        file_path = self.build_generation_path(file_name="models.py")
         templates_path = os.path.join(os.path.dirname(
             os.path.abspath(__file__)), "templates")
-        if self.output_dir != None:
-            file_path = os.path.join(self.output_dir, file_name)
-        else:
-            os.makedirs(os.path.join(working_path, "output"), exist_ok=True)
-            file_path = os.path.join(working_path, "output", file_name)
         env = Environment(loader=FileSystemLoader(
             templates_path), trim_blocks=True, lstrip_blocks=True, extensions=['jinja2.ext.do'])
         template = env.get_template('django_template.py.j2')

afc2d7ebd8dccae1bb11445067c1e7d220dd0be2
FitashUlHaq <fitashulhaq@hotmail.com>
2024-02-14 16:12:22 +0100

    added metamodel for OD

---
+++ besser/BUML/metamodel/structural/ObjectDiagram.py
@@ -0,0 +1,30 @@
+from besser.BUML.metamodel.structural import NamedElement, Property, Type,Association
+
+class AttributeLink:
+    def __init__(self):
+        self.attribute = Property()
+class Instance(NamedElement):
+    def __init__(self):
+        self.classifier: list[Type]
+        self.slot: list[AttributeLink]
+        self.ownedLink: list[Link]
+        self.linkEnd : list[LinkEnd]
+
+class Object(Instance):
+    def __init__(self):
+        super().__init__()
+        self.value = ""
+
+class DataValue(Instance):
+    def __init__(self):
+        super().__init__()
+        self.value = ""
+
+
+class Link(NamedElement):
+    def __init__(self):
+        self.connection = list[LinkEnd]
+class LinkEnd(NamedElement):
+    def __init__(self):
+        self.associationEnd = list[Property]
+

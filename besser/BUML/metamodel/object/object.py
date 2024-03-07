c260a845f88e11df1aa10178f005df4efd401ef4
Ivan David ALFONSO DIAZ <ivandaalf@hotmail.com>
2024-03-07 15:19:52 +0100

    update object metamodel

--- besser/BUML/metamodel/object/object.py
+++ besser/BUML/metamodel/object/object.py
@@ -1,7 +1,7 @@
 import string
 from besser.BUML.metamodel.structural import NamedElement, Property, Type, Association
 
-class AttributeLink(NamedElement):
+class AttributeLink():
     """An attribute link is a named slot in an instance, which holds the value of an attribute
     
     Args:
@@ -10,13 +10,12 @@ class AttributeLink(NamedElement):
         attribute (Property): the attribute or property from the structural metamodel.
     
     Attributes:
-        name (str): Inherited from NamedElement, represents the name of the association.
+        name (str): inherited from NamedElement, represents the name of the attribute link.
         value (DataValue): the value of the attribute.
         attribute (Property): the attribute or property from the structural metamodel.
     """
 
-    def __init__(self, name, value: "DataValue", attribute: Property):
-        super().__init__(name)
+    def __init__(self, value: "DataValue", attribute: Property):
         self.value: DataValue = value
         self.attribute: Property = attribute
 
@@ -27,17 +26,17 @@ class AttributeLink(NamedElement):
 
     @value.setter
     def value(self, value: "DataValue"):
-        """bool: Set the value of the attribute."""
+        """DataValue: Set the value of the attribute."""
         self.__value = value
 
     @property
     def attribute(self) -> Property:
-        """DataValue: Get the attribute."""
+        """Property: Get the attribute."""
         return self.__attribute
 
     @value.setter
     def attribute(self, attribute: Property):
-        """bool: Set the attribute."""
+        """Property: Set the attribute."""
         self.__attribute = attribute
 
     def __repr__(self) -> str:
@@ -46,62 +45,80 @@ class AttributeLink(NamedElement):
 class Instance(NamedElement):
     """The instance defines an entity to which a set of operations can be applied and which has a state that stores the effects of the operations.
     
+    Args:
+        name (str): the name of the instance
+        classifier (Type): the classifier of the instance. It could be for example a Class or a PrimitiveDataType of the structural metamodel.
+
     Attributes:
-        classifier: classname of the instance
-        slots: list of properties
-        ownedLink: the set of Links that are owned by the Instance.
-        linkEnd: The set of LinkEnds of the connected Links that are attached to the Instance.
-           """
+        name (str): inherited from NamedElement, represents the name of the instance.
+        classifier (Type): the classifier of the instance. It could be for example a Class or a PrimitiveDataType of the structural metamodel.
+    """
 
-    def __init__(self):
-        self.classifier:Type = None
-        self.slots = None
-        self.ownedLink = None
-        self.linkEnd = None
+    def __init__(self, name: str, classifier: Type):
+        super().__init__(name)
+        self.classifier: Type = classifier
 
+    @property
+    def classifier(self) -> Type:
+        """Type: Get the classifier."""
+        return self.__classifier
 
+    @classifier.setter
+    def classifier(self, classifier: Type):
+        """Type: Set the classifier."""
+        self.__classifier = classifier
 
 class Object(Instance):
     """ An object is an instance that originates from a class.
-     Attributes:
-          slots: list of properties
-          linkEnd: The set of LinkEnds of the connected Links that are attached to the
-Instance.
+    
+    Args:
+        name (str): the name of the object instance
+        classifier (Type): the classifier of the object instance. It could be for example a Class or a PrimitiveDataType of the structural metamodel.
+        slots (list[AttributeLink]): list of properties of the instance
+        link_ends (list[LinkEnd]): The set of LinkEnds of the connected Links that are attached to the Instance.
+    
+    Attributes:
+        name (str): inherited from NamedElement, represents the name of the object instance.
+        classifier (Type): Inherited from Instance, represents the classifier of the object.
+        slots (list[AttributeLink]): list of properties of the instance
+        link_ends (list[LinkEnd]): The set of LinkEnds of the connected Links that are attached to the Instance.
     """
-    def __init__(self):
-        super().__init__()
-        self.slots: list[AttributeLink] = []
-        self.linkEnd: list[LinkEnd] = []
-    def add_slot(self, slot):
+    def __init__(self, name: str, classifier: Type, slots: list[AttributeLink] = []):
+        super().__init__(name, classifier)
+        self.slots: list[AttributeLink] = slots
+
+    @property
+    def slots(self) -> list[AttributeLink]:
+        """list[AttributeLink]: Get the slots."""
+        return self.__slots
+
+    @slots.setter
+    def slots(self, slots: list[AttributeLink]):
+        """list[AttributeLink]: Set the slots."""
+        self.__slots = slots
+
+    def add_slot(self, slot: AttributeLink):
         """ Method to add attribute link to slots"""
         self.slots.append(slot)
 
-    def add_to_link_end(self, link):
-        """ Method to add link to linkend"""
-
-        self.linkEnd.append(link)
-
-    def get_slots(self):
-        """ slots: Method to retrieve slots"""
-
-        return self.slots
-
     def __repr__(self):
-         """str: printing the value of object."""
-         toRet =""
-         for s in self.slots:
-            toRet = toRet + str(s) +'\n'
-         return toRet
+         return f'Object({self.name}, {self.classifier}, {self.slots}, {self.link_ends})'
 
 
 class DataValue(Instance):
-    """ An DataValue is an instance with no identity.
-         Attributes:
-              value: value of the property
-    Instance.
-        """
-    def __init__(self,value):
-        super().__init__()
+    """ An DataValue represent the value of a property or attribute of an Object.
+    
+    Args:
+        classifier (Type): the classifier of the DataValue. It could be for example a Class or a PrimitiveDataType of the structural metamodel.
+        value: value of the property Instance.
+    
+    Attributes:
+        classifier (Type): Inherited from Instance, represents the classifier of the DataValue instance.
+        value: value of the property Instance.
+    """
+
+    def __init__(self, classifier: Type, value, name=""):
+        super().__init__(name, classifier)
         self.__value = value
 
     @property
@@ -109,34 +126,89 @@ class DataValue(Instance):
        """Method to retrieve Value"""
        return self.__value
 
-
     @value.setter
     def value(self, val):
         """Method to set Value"""
-
         self.__value = val
 
 class LinkEnd(NamedElement):
     """ A link end is an end point of a link.
-    args:
-    name : name of the link
-    ins: Instance of the link
+
+    Args:
+        name (str): the name of the LinkEnd
+        association_end (Property): the end represeted by the LinkEnd
+        object (Object): the object pointed to by the LinkEnd
+    
     Attributes:
-        associationEnd: property pointing to link
-        instance : instance of link
-            """
-    def __init__(self,name,ins):
+        name (str): inherited from NamedElement, represents the name of the LinkEnd
+        association_end (Property): the end of the link
+        object (Object): the object pointed to by the LinkEnd
+    """
+
+    def __init__(self, name:str, association_end: Property, object: Object):
         super().__init__(name)
-        self.associationEnd: Property = None
-        self.instance= ins
+        self.association_end: Property = association_end
+        self.object: Object = object
+
+    @property
+    def association_end(self):
+       """Property: Method to retrieve the association end"""
+       return self.__association_end
+
+    @association_end.setter
+    def association_end(self, association_end: Property):
+        """Property: Method to set the association end"""
+        self.__association = association_end
+
+    @property
+    def object(self):
+       """Object: Method to retrieve the object"""
+       return self.__association
+
+    @object.setter
+    def object(self, object: Object):
+        """Object: Method to set the object"""
+        self.__object = object
 
 class Link(NamedElement):
-    """ A link contains list of linkends.
-    attributes:
-    connection: list of linkends
+    """ A link represent a relationship between objects.
+
+    Args:
+        name (str): the name of the Link
+        association (Association): the Association that represents the Link
+        connections: list of link ends.
+    
+    Attributes:
+        name (str): inherited from NamedElement, represents the name of the Link
+        association (Association): the Association that represents the Link
+        connections: list of link ends.
     """
-    def __init__(self):
-        self.connection : list[LinkEnd] = []
+
+    def __init__(self, name: str, association: Association, connections: list[LinkEnd]):
+        super().__init__(name)
+        self.association: Association = association
+        self.connection: list[LinkEnd] = connections
+    
+    @property
+    def association(self):
+       """Association: Method to retrieve the association"""
+       return self.__association
+
+    @association.setter
+    def association(self, association: Association):
+        """Association: Method to set the association"""
+        self.__association = association
+
+    @property
+    def connections(self):
+       """list[LinkEnd]: Method to retrieve the connections"""
+       return self.__connections
+
+    @connections.setter
+    def connections(self, connections: list[LinkEnd]):
+        """list[LinkEnd]: Method to set the connections"""
+        self.__connections = connections
+
     def add_to_connection(self,linkEnd):
         """Method to add linkend"""
         self.connection.append(linkEnd)

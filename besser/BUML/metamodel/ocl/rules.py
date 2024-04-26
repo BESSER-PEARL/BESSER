8fc8eefba9ae43f0ff6a0ed6b4b5f1dbe65b51b9
ivan-alfonso <ivandaalf@hotmail.com>
2024-04-26 13:31:34 +0200

    Add ReadTheDocs documentation for OCL api

--- besser/BUML/metamodel/ocl/rules.py
+++ besser/BUML/metamodel/ocl/rules.py
@@ -23,10 +23,12 @@ class OCLExpression(TypedElement):
     def source(self) ->Any:
         """Get the source of OCL Expression"""
         return self._source
+    
     @source.setter
     def source(self, source)->Any:
         """Set the source of OCL Expression"""
         self._source = source
+
     @property
     def referredOperation(self)->Any:
         """Get the referredOperation of OCL Expression"""
@@ -40,8 +42,8 @@ class OCLExpression(TypedElement):
     def __str__(self) -> str:
         pass
 
-
-""" A literal value part of an OCL expression
+class LiteralExpression(OCLExpression):
+    """ A literal value part of an OCL expression
 
     Args:
         name (str): the name of the expression
@@ -51,7 +53,7 @@ class OCLExpression(TypedElement):
     Attributes:
         value: value of the expression
     """
-class LiteralExpression(OCLExpression):
+
     def __init__(self, name: str, type: Type, value: Any):
         super().__init__(name, type)
         self.value: Any = value
@@ -72,27 +74,28 @@ class LiteralExpression(OCLExpression):
 class IntegerLiteralExpression(LiteralExpression):
     """ A Integer literal Expression of type LiteralExpression
 
-        Args:
-            name (str): the name of the expression
-            value: value of the expression
-        """
+    Args:
+        name (str): the name of the expression
+        value: value of the expression
+    """
+
     def __init__(self, name: str, value: int):
         super().__init__(name, type=PrimitiveDataType(name="int"), value=value)
 
     def __repr__(self):
         return f'IntegerLiteralExpression({self.value})'
 
-
-# Expression that returns the value of a given Property
 class PropertyCallExpression(OCLExpression):
     """ A Property Call Expression of type OCLExpression
 
-        Args:
-            name (str): the name of the expression
-            property: property of the expression
-        Attributes:
-            property: Property of the expression
-        """
+    Args:
+        name (str): the name of the expression
+        property: property of the expression
+
+    Attributes:
+        property: Property of the expression
+    """
+
     def __init__(self, name: str, property: Property):
         super().__init__(name, Type(property.type))
         self.property: Property = property
@@ -113,24 +116,24 @@ class PropertyCallExpression(OCLExpression):
         """Set the Property of Expression"""
         self.__property = property
 
-
-
 class OperationCallExpression(OCLExpression):
     """
     Expression that returns the value of a given Operation on a given set of ordered arguments
     The operation could also be a reference to any operation in an existing class but for now we stick to simple
-    arithmetic comparison operations from the standard OCL library
+    arithmetic comparison operations from the standard OCL library.
 
-    An Operation Call Expression of type OCL expression:
+    An Operation Call Expression of type OCL expression.
+
     Args:
         name : Name of the function
         operation: Operation of the expression
         arguments: Arguments of the function
+
     Attributes:
         operation: Operation of the expression
         arguments: Arguments of the function
-
     """
+
     def __init__(self, name: str, operation: str, arguments: list[OCLExpression]):
         super().__init__(name, Type(PrimitiveDataType("bool")))  # Type for now is always boolean, it should be the return type of the operation
         self.operation: str = operation
@@ -169,28 +172,31 @@ class OperationCallExpression(OCLExpression):
         return toRet
         # return f'{self.arguments[0]} {self.operation} {self.arguments[1]}'
 
-# A class to represents OCL constriants, i.e. constraints written with the OCL language
 class OCLConstraint(Constraint):
     """A class to represents OCL constriants, i.e. constraints written with the OCL language
+
     Args:
         name: name of constraint
         context: class of constraint
         expression: expression of constraint
         language: Language of constraint
     """
+
     def __init__(self, name: str, context: Class, expression: OCLExpression, language: str = "OCL"):
         super().__init__(name, context, expression, language)
 
 
 class IfExp(OCLExpression):
     """A class to represents IfExpressions
-        Args:
-            name: name of constraint
-            type: type of constraint
-            ifcond: If Condition of constraint
-            elseExp: Else expression of constraint
-            then Exp: then expression of constraint
-        """
+
+    Args:
+        name: name of constraint
+        type: type of constraint
+        ifcond: If Condition of constraint
+        elseExp: Else expression of constraint
+        then Exp: then expression of constraint
+    """
+
     def __init__(self, name: str, type: Type,ifcond = None, elseExp = None, thenExp = None,):
         # self.ifOwner = null
         super().__init__(name, type)
@@ -208,6 +214,7 @@ class IfExp(OCLExpression):
     def elseCondition(self) -> OCLExpression:
         """Get the else Condition of expression"""
         return self._else_expression
+    
     @property
     def thenExpression(self) ->OCLExpression:
         """Get the then expression of OCLexpression"""
@@ -222,19 +229,24 @@ class IfExp(OCLExpression):
     def elseCondition(self,else_cond):
         """Set the else Condition of expression"""
         self._else_expression = else_cond
+
     @thenExpression.setter
     def thenExpression(self, then_expression):
         """Set the then expression of OCLexpression"""
         self._then_expression = then_expression
+
 class VariableExp(OCLExpression):
     """A class to define the variable expression of the type OCL expression
+
     Args:
         name: Name of the expression
         type: Type of the expression
+
     Attributes:
         name: name of the expression
         variable: variable of the expression
     """
+
     def __init__(self,name: str, type: Type):
         super().__init__(name, type)
         self.name = name
@@ -244,6 +256,7 @@ class VariableExp(OCLExpression):
         """Set the value of variable in the expression"""
         val.replace('self.','')
         self.variable.set_value(val)
+
     def getVal(self):
         """Get the value of variable in the expression"""
         return self.variable.get_value()
@@ -253,10 +266,12 @@ class VariableExp(OCLExpression):
 
 class Variable(OCLExpression):
     """A class to define the variable in the OCL expression
+
     Args:
         name: name of the expression
         type: type of the expression
     """
+
     def __init__(self,name: str, type: Type):
         super().__init__(name, type)
 
@@ -269,24 +284,30 @@ class Variable(OCLExpression):
 
 
 class TypeExp(OCLExpression):
-        """ A class to define the type expression of the type OCL Expressions
+    """ A class to define the type expression of the type OCL Expressions
+
     Args:
         name: name of the classifier
         type: type of expressions
+
     Attributes:
         referedType: classifier of the expression
     """
-        def __init__(self,name: str, type: Type):
-            super().__init__(name, type)
 
-            self.referedType = Classifier(name)
+    def __init__(self,name: str, type: Type):
+        super().__init__(name, type)
+        self.referedType = Classifier(name)
+
 class Parameter:
     """A class to define parameter
+
     Args:
         val: Value of the parameter
+
     Attributes:
         value: Value of the parameter
     """
+
     def __init__(self,val):
         self.value =val
 
@@ -296,29 +317,35 @@ class Parameter:
 
 
 class StateExp(OCLExpression):
-        """A class to define the state expressions of type OCL expression
-        Args:
-            name: name of the expression
-            type: type of the expression
-        Attributes:
-            ReferedState: Referred state of the expression
-        """
-        def __init__(self,name: str, type: Type):
-            super().__init__(name, type)
+    """A class to define the state expressions of type OCL expression
 
-            self.referedState = State()
+    Args:
+        name: name of the expression
+        type: type of the expression
+
+    Attributes:
+        ReferedState: Referred state of the expression
+    """
+
+    def __init__(self,name: str, type: Type):
+        super().__init__(name, type)
+
+        self.referedState = State()
+
 class State:
     """A class to define state"""
+
     def __init__(self):
         self.stateExp = []
 
 class RealLiteralExpression(LiteralExpression):
     """ A Real literal Expression of type LiteralExpression
 
-            Args:
-                name (str): the name of the expression
-                value: value of the expression
-            """
+    Args:
+        name (str): the name of the expression
+        value: value of the expression
+    """
+
     def __init__(self, name: str, value: float):
         super().__init__(name, type=PrimitiveDataType(name="float"), value=value)
 
@@ -328,41 +355,47 @@ class RealLiteralExpression(LiteralExpression):
 
 class Classifier:
     """ A class to define classifier
-            Args:
-                name (str): the name of the classifier
-            """
+
+    Args:
+        name (str): the name of the classifier
+    """
+
     def __init__(self, name= None):
         self.name = name
     pass
+
 class CallExp(OCLExpression):
     """ A class to define call expression"""
     pass
+
 class FeatureCallExp(CallExp):
     """ A class to define feature call expression"""
-
     pass
+
 class LiteralExp(OCLExpression):
     """ A class to define Literal expression"""
-
     pass
+
 class InvalidLiteralExp(LiteralExp):
     """ A class to define invalid literal expression"""
-
     pass
+
 class LoopExp(CallExp):
     """ A class to define loop expression
+
     Args:
         name: name of the expression
         type: type of the expression
+
     Attributes:
         name: name of the expression
         type: type of the expression
         body: a list to store expressions in the body of loop expression
         iterator: list to store all the iterators
     """
+
     def __init__(self,name: str, type: Type):
         super().__init__(name, type)
-
         self.name = name
         self.type = type
         self.body = []
@@ -376,20 +409,22 @@ class LoopExp(CallExp):
             stringToRet = stringToRet + "\nbody: " + str(body)
         return stringToRet
 
-
     def add_body(self,body):
         """Add expression to the body"""
         self.body.append(body)
+
     @property
     def get_body(self):
         """Get body of the expression"""
         return self.body
+    
     def addIterator(self,iterator):
         """add iterator to the expression"""
         self.iterator.append(iterator)
     # @property
     # def set_iterator (self,iterator):
     #     self.iterator = iterator
+        
     @property
     def get_iterator(self):
         """add iterator of the expression"""
@@ -399,32 +434,33 @@ class LoopExp(CallExp):
 class MessageExp(OCLExpression):
     """A class to define message expression"""
     pass
+
 class NavigationCallExp(OCLExpression):
     """A class to define navigation call expression"""
-
     pass
+
 class NullLiteralExp(LiteralExp):
     """A class to define null literal expression"""
-
     pass
 
 class PrimitiveLiteralExp(LiteralExp):
     """A class to define primitive literal expression"""
-
     pass
+
 class NumericLiteralExp(PrimitiveLiteralExp):
     """A class to define numeric literal expression"""
-
     pass
+
 class IterateExp(LoopExp):
     """A class to define Iterate expression
+    
     Args:
         name: name of expression
         type: type of expression
+
     Attributes:
         result: variable to store the result
-        """
-
+    """
 
     def __init__(self,name: str, type: Type):
         super().__init__(name, type)
@@ -433,9 +469,11 @@ class IterateExp(LoopExp):
 
 class IteratorExp(LoopExp):
     """A class to define Iterator expression
+
     Args:
         name: name of expression
         type: type of expression
+
     Attributes:
         name: name of expression
         type: type of expression
@@ -446,41 +484,49 @@ class IteratorExp(LoopExp):
         self.name = name
         self.type = type
     pass
+
     def __str__(self):
         return self.name +":" +self.type
 
 class LetExp(OCLExpression):
     """A class to define Let expression
-        Args:
-            name: name of expression
-            type: type of expression
-        Attributes:
-            OCLExpression: OCL expression of the LetExp
-            variable: variable of the Let expression
-        """
+
+    Args:
+        name: name of expression
+        type: type of expression
+
+    Attributes:
+        OCLExpression: OCL expression of the LetExp
+        variable: variable of the Let expression
+    """
+
     def __init__(self, name: str, type: Type):
         super().__init__(name, type)
         self.OCLExpression = None
         self.variable = None
+
 class BooleanLiteralExpression(LiteralExpression):
     """ A Boolean literal Expression of type LiteralExpression
 
-                Args:
-                    name (str): the name of the expression
-                    value: value of the expression
-                """
+    Args:
+        name (str): the name of the expression
+        value: value of the expression
+    """
+
     def __init__(self, name: str, value: bool):
         super().__init__(name, type=PrimitiveDataType(name="bool"), value=value)
 
     def __repr__(self):
         return f'BooleanLiteralExpression({self.value})'
+    
 class StringLiteralExpression(LiteralExpression):
     """ A String literal Expression of type LiteralExpression
 
-                Args:
-                    name (str): the name of the expression
-                    value: value of the expression
-                """
+    Args:
+        name (str): the name of the expression
+        value: value of the expression
+    """
+
     def __init__(self, name: str, value: str):
         super().__init__(name, type=PrimitiveDataType(name="str"), value=value)
 
@@ -492,128 +538,157 @@ class InfixOperator:
 
     Args:
         operator: Operator of the expression
+
     Attributes:
         operator: Operator of the expression
+    """
 
-                                    """
     def __init__(self,operator):
         self.operator = operator
+
     def get_infix_operator(self):
         """Get the infinix Operator"""
-
         return self.operator
+    
     def __str__(self):
         return self.operator
 
 class DataType(Classifier):
     """A class to define the data type
+
     Args:
         name: Name of the expression
     """
+
     def __init__(self,name):
         super().__init__(name)
 
 class CollectionType(DataType):
     """A class to define the Collection Type
-        Args:
-            name: Name of the expression
-        """
+
+    Args:
+        name: Name of the expression
+    """
+    
     def __init__(self,name):
         super().__init__(name)
 
 class OrderedSetType(CollectionType):
     """A class to define the Ordered Set Type
-            Args:
-                name: Name of the expression
-            """
+
+    Args:
+        name: Name of the expression
+    """
+
     def __init__(self,name):
         super().__init__(name)
 
 class SequenceType(CollectionType):
     """A class to define the Sequence Type
-            Args:
-                name: Name of the expression
-            """
+
+    Args:
+        name: Name of the expression
+    """
+
     def __init__(self,name):
         super().__init__(name)
 
 class BagType(CollectionType):
     """A class to define the Bag Type
-            Args:
-                name: Name of the expression
-            """
+
+    Args:
+        name: Name of the expression
+    """
+
     def __init__(self,name):
         super().__init__(name)
 
 class SetType(CollectionType):
     """A class to define the Set Type
-            Args:
-                name: Name of the expression
-            """
+
+    Args:
+        name: Name of the expression
+    """
+
     def __init__(self,name):
         super().__init__(name)
 
 
 class CollectionLiteralExp(LiteralExp):
     """A class to define the Collection Literal Expression
-            Args:
-                name: Name of the expression
-                type: type of expression
-            Attributes:
-                kind: type of expression
-                collectionItems: Items in the collection literal expression
-            """
+
+    Args:
+        name: Name of the expression
+        type: type of expression
+
+    Attributes:
+        kind: type of expression
+        collectionItems: Items in the collection literal expression
+    """
+
     def __init__(self, name, type):
         super().__init__(name,type)
         self.kind = type
         self.collectionItems = []
+
     def __str__(self):
         toRet= str(self.kind) +": "
         for item in self.collectionItems:
             toRet = toRet + str(item)
         return toRet
+    
     def add(self, item):
         """Method to add item in the collection items"""
         self.collectionItems.append(item)
 
 class CollectionLiteralPart(TypedElement):
     """A class to define the Collection Literal Part
-                Args:
-                    name: Name of the expression
-                """
+
+    Args:
+        name: Name of the expression
+    """
+
     def __init__(self, name):
         super().__init__(name,type = "NP")
 
 
 class CollectionItem(CollectionLiteralPart):
     """A class to define the Collection Item
+
     Args:
         name: Name of the expression
+
     Attributes:
-        value: value of the item
+        value: Value of the item
     """
+
     def __init__(self, name,item):
         super().__init__(name)
         self.value = item
+
     def set(self,  value):
         """set the value of item"""
         self.value = value
+
     def __str__(self):
         return str(self.value)+","
+    
     def get(self):
         """Get the value of item"""
         return self.value
+    
 class CollectionRange(CollectionLiteralPart):
-    """Ä class to define collection range
+    """A class to define collection range
+
     Args:
         name: name of expression
+
     Attributes:
         first: first item of collection
         last: last item of collection
     """
+
     def __init__(self, name):
         super().__init__(name)
         self.first = None
         self.last = None
-
-

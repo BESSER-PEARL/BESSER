382367ee0ea98e9d5f84c3f68ac67f002b16905a
ivan-alfonso <ivandaalf@hotmail.com>
2024-05-23 14:09:43 +0200

    Fix on_delete bug for Django generator

--- tests/structural/library/output/models.py
+++ tests/structural/library/output/models.py
@@ -5,7 +5,7 @@ class Book(models.Model):
     release = models.DateTimeField()
     title = models.CharField(max_length=255)
     writtenBy = models.ManyToManyField('Author')
-    locatedIn = models.ForeignKey('Library')
+    locatedIn = models.ForeignKey('Library', on_delete=models.PROTECT)
 
     def __str__(self):
         return str(self.id)

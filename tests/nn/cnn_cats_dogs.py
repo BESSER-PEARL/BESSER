6b238004c07eb57398231baec3541d26d20d4ba6
DaoudiNadia <daoudi.nadia.dn@gmail.com>
2024-09-23 16:15:21 +0200

    update cnn_cats_dogs.py file

--- tests/nn/cnn_cats_dogs.py
+++ tests/nn/cnn_cats_dogs.py
@@ -2,6 +2,7 @@ from besser.BUML.metamodel.nn import NN, Conv2D, PoolingLayer, \
     FlattenLayer, LinearLayer, Parameters, Label, Image, TrainingDataset, \
     TestDataset
 from besser.generators.pytorch.pytorch_code_generator import PytorchGenerator
+from besser.generators.tf.tf_code_generator import TFGenerator
 
 nn_model: NN = NN(name="my_model")
 nn_model.add_layer(Conv2D(name="l1", actv_func="relu", in_channels=3, 
@@ -26,11 +27,10 @@ nn_model.add_layer(LinearLayer(name="l7", actv_func="relu", in_features=120,
 nn_model.add_layer(LinearLayer(name="l8", actv_func="sigmoid", in_features=84, 
                                out_features=1))
 
-parameters: Parameters = Parameters(batch_size=16, epochs=30, 
+parameters: Parameters = Parameters(batch_size=16, epochs=2, 
                                     learning_rate=0.01, optimiser="adam", 
                                     metrics=["f1-score"], 
-                                    loss_function="binary_crossentropy", 
-                                    weight_decay=0.00001)
+                                    loss_function="binary_crossentropy")
 
 nn_model.add_parameters(parameters)
 
@@ -47,5 +47,9 @@ test_data = TestDataset(name="test_data",
                         path_data=r"dataset\images_cats_dogs\test")
 
 pytorch_model = PytorchGenerator(model=nn_model, train_data=train_data, 
-                                 test_data=test_data, output_dir="output/cnn")
+                                 test_data=test_data, output_dir="output/cnn_cats")
 pytorch_model.generate() 
+
+tf_model = TFGenerator(model=nn_model, train_data=train_data, 
+                       test_data=test_data, output_dir="output/cnn_cats")
+tf_model.generate() 

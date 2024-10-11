6ec63db1a6d12af6eb12218e7f46faf2cff0ca8d
Nadia DAOUDI <nadia.daoudi@list.lu>
2024-10-11 16:13:32 +0200

    update tf code generator

--- besser/generators/tf/tf_code_generator.py
+++ besser/generators/tf/tf_code_generator.py
@@ -1,9 +1,9 @@
 import os
 from jinja2 import Environment, FileSystemLoader
-from besser.BUML.metamodel.nn import NN, TrainingDataset, TestDataset
+from besser.BUML.metamodel.nn import NN
 from besser.generators import GeneratorInterface
-from besser.generators.tf.utils import get_layer_notation, get_tensorop_notation, \
-     add_notation_to_sub_nn
+from besser.generators.tf.utils import get_layer_in_out_variables, get_tensorop_out_variable, \
+     add_in_out_variable_to_subnn
 from typing import Dict
 
 class TFGenerator(GeneratorInterface):
@@ -13,45 +13,20 @@ class TFGenerator(GeneratorInterface):
 
     Args:
         model (NN): An instance of the NN Model class representing the B-UML model.
-        train_data (TrainingDataset): An instance of the training dataset class representing the dataset used to train the NN model.
-        test_data (TestDataset): An instance of the test dataset class representing the dataset used to evaluate the NN model.
         output_dir (str, optional): The output directory where the generated code will be saved. Defaults to None.
     """
-    def __init__(self, model: NN, train_data: TrainingDataset = None, test_data: TestDataset = None,
-                 output_dir: str = None):
+    def __init__(self, model: NN, output_dir: str = None):
         super().__init__(model, output_dir)
-        self.train_data: TrainingDataset = train_data
-        self.test_data: TestDataset = test_data
         self.modules_details: Dict = self.get_modules_details()
 
         @property
-        def train_data(self) -> TrainingDataset:
-            """TrainingDataset: Get the dataset used to train the NN model."""
-            return self.__train_data
-    
-        @train_data.setter
-        def train_data(self, train_data: TrainingDataset):
-            """TrainingDataset: Set the dataset used to train the NN model."""
-            self.__train_data = train_data
-
-        @property
-        def test_data(self) -> TestDataset:
-            """TestDataset: Get the dataset used for evaluating the NN model."""
-            return self.__test_data
-    
-        @test_data.setter
-        def test_data(self, test_data: TestDataset):
-            """TestDataset: Set the dataset used for evaluating the NN model."""
-            self.__test_data = test_data
-
-        @property
         def modules_details(self) -> Dict:
-            """Dict: Get the dict that contains the names of layers or sub_nns or tensorops and their syntax and notation."""
+            """Dict: Get the dict that contains the names of modules, their syntax and their input and output tensor variables."""
             return self.__modules_details
     
         @modules_details.setter
         def modules_details(self, modules_details: Dict):
-            """Dict: Set the dict that contains the names of layers or sub_nns or tensorops and their syntax and notation."""
+            """Dict: Set the dict that contains the names of modules, their syntax and their input and output tensor variables."""
             raise AttributeError("modules_details attribute is read-only")
 
 
@@ -62,15 +37,15 @@ class TFGenerator(GeneratorInterface):
             if module.__class__.__name__ == "NN":
                 sub_nn_modules_details = {}
                 for sub_nn_layer in module.layers:
-                    sub_nn_modules_details = get_layer_notation(sub_nn_layer, sub_nn_modules_details)
+                    sub_nn_modules_details = get_layer_in_out_variables(sub_nn_layer, sub_nn_modules_details)
                 name_sub_nn = module.name + "_" + str(counter_sub_nn) + "_nn" 
                 modules_details[name_sub_nn] = sub_nn_modules_details
                 counter_sub_nn += 1
-                modules_details = add_notation_to_sub_nn(modules_details)
+                modules_details = add_in_out_variable_to_subnn(modules_details)
             elif module.__class__.__name__ != "TensorOp":
-                modules_details = get_layer_notation(module, modules_details)
+                modules_details = get_layer_in_out_variables(module, modules_details)
             else:
-                modules_details = get_tensorop_notation(module, modules_details)
+                modules_details = get_tensorop_out_variable(module, modules_details)
         return modules_details
     
     def build_generation_path(self, file_name:str) -> str:
@@ -100,6 +75,6 @@ class TFGenerator(GeneratorInterface):
         template = env.get_template(f"template_tf_functional.py.j2")
 
         with open(file_path, mode="w") as f:
-            generated_code = template.render(model=self.model, train_data=self.train_data, test_data=self.test_data, modules_details=self.modules_details)
+            generated_code = template.render(model=self.model, modules_details=self.modules_details)
             f.write(generated_code)
             print("Code generated in the location: " + file_path)
--- besser/generators/tf/utils.py
+++ besser/generators/tf/utils.py
@@ -7,21 +7,22 @@ import random
 from PIL import Image
 import numpy as np
 
+modules_details = dict()
+
 """
 A module can be a layer, a sub_nn or a tensorop.
 This dict is created to keep track of the syntax of the modules,
-the notation in the forward method of their input, and the notation
-of the their output in the forward method.
-It has this structure:
-{"name_module": [syntax, notation_out, notation_in]}
+their tensor input variable, and their tensor output variable 
+in the forward method. It has this structure:
+{"name_module": [syntax, out_var, in_var]}
 syntax: The syntax of calling the module.
-notation_out: the notation of the output of the module.
-notation_in: the notation of the input of module.
-Example: {"l2": ["self.l2 = nn.Linear(in_features=32, out_features=10)", "x_1", "x"]}
+out_var: the output tensor variable of the module.
+in_var: the input tensor variable of module.
+Example: {"l2": ["self.l2 = layers.Dense(units=40, activation='relu')", "x_1", "x"]}
 PS: for the case of layers, an additional element is added to the list, representing the layer object.
 """
 
-modules_details = dict()
+
 
 def setup_general_layer(layer: Layer) -> str:
     actv_func = setup_activation_function(layer)
@@ -115,32 +116,32 @@ def setup_cnn(layer: Layer) -> str:
 
 
 
-def get_input_notation(layer, modules_details, prev_out_notation):
+def get_input_variable(layer, modules_details, prev_out_variable):
     my_keys = list(modules_details.keys())
     if layer.name_layer_input != None:
         if layer.name_layer_input+"_layer" in my_keys:
             return modules_details[layer.name_layer_input + "_layer"][1]
         elif layer.name_layer_input+"_nn" in my_keys:
-            return  modules_details[layer.name_layer_input + "_nn"]["notation"]
+            return  modules_details[layer.name_layer_input + "_nn"]["in_out_variable"]
         #module.name_layer_input+"_op" in my_keys:
         else:
             return modules_details[layer.name_layer_input + "_op"][1]
     else:
-        return prev_out_notation
+        return prev_out_variable
         
-def get_layers_notation_for_tensors(layers_names, modules_details):
+def get_layers_output_for_tensorops(layers_names, modules_details):
     my_keys = list(modules_details.keys())
-    notations = []
+    out_variables = []
     for layer_name in layers_names:
         if layer_name+"_layer" in my_keys:
-            notations.append(modules_details[layer_name + "_layer"][1])
+            out_variables.append(modules_details[layer_name + "_layer"][1])
         else:
-            notations.append(modules_details[layer_name + "_op"][1])
-    return notations
+            out_variables.append(modules_details[layer_name + "_op"][1])
+    return out_variables
 
-def get_previous_out_notation(modules_details, previous_module):
+def get_previous_out_variable(modules_details, previous_module):
     if isinstance(modules_details[previous_module], dict):
-        return modules_details[previous_module]["notation"]
+        return modules_details[previous_module]["in_out_variable"]
     else:
         return modules_details[previous_module][1]
     
@@ -158,57 +159,57 @@ def get_layer_syntax(layer, modules_details):
     return layer_synt, modules_details
   
 
-def add_notation_to_sub_nn(modules_details):
+def add_in_out_variable_to_subnn(modules_details):
     previous_module = list(modules_details.keys())[-1]
     if previous_module.endswith("nn"):
         if len(modules_details) == 1:
-            notation_nn = "x"
+            in_out_variable = "x"
         elif isinstance(list(modules_details.values())[-2], dict):
-            notation_nn = list(modules_details.values())[-2]["notation"]
+            in_out_variable = list(modules_details.values())[-2]["in_out_variable"]
         else:
-            # get the notation of the layer before the sub_nn
-            notation_nn = list(modules_details.values())[-2][1]
-        modules_details[previous_module]["notation"] = notation_nn
+            # get the output variable of the layer before the sub_nn
+            in_out_variable = list(modules_details.values())[-2][1]
+        modules_details[previous_module]["in_out_variable"] = in_out_variable
     return modules_details
 
 
-def get_tensorop_notation_empty_dict(tensorOp):
+def get_tensorop_output_empty_dict(tensorOp):
     if tensorOp.input_reused == True:
-        notation_ts_out = "x_1"
+        out_variable = "x_1"
     else:
-        notation_ts_out = "x"
-    return notation_ts_out
+        out_variable = "x"
+    return out_variable
 
 
-def get_layer_notation_empty_dict(layer):
+def get_layer_variable_empty_dict(layer):
     if layer.input_reused == True:
-        notation_layer_out, notation_layer_in = "x_1", "x"
+        out_variable_layer, in_variable_layer = "x_1", "x"
     else:
-        notation_layer_out, notation_layer_in = "x", "x"
-    return notation_layer_out, notation_layer_in
+        out_variable_layer, in_variable_layer = "x", "x"
+    return out_variable_layer, in_variable_layer
 
-def get_layer_notation_input_reused(layer, prev_out_notation, modules_details):
-    if prev_out_notation == "x":
-        notation_layer_out = "x_1"
+def get_layer_variable_input_reused(layer, prev_out_variable, modules_details):
+    if prev_out_variable == "x":
+        out_variable_layer = "x_1"
     else:
-        notation_layer_out = f"x_{int(prev_out_notation.split('_')[-1])+1}"
-    notation_layer_in = get_input_notation(layer, modules_details, prev_out_notation)
-    return notation_layer_out, notation_layer_in
+        out_variable_layer = f"x_{int(prev_out_variable.split('_')[-1])+1}"
+    in_variable_layer = get_input_variable(layer, modules_details, prev_out_variable)
+    return out_variable_layer, in_variable_layer
 
-def get_layer_notation_input_not_reused(layer, prev_out_notation, modules_details):   
-    notation_layer_out = prev_out_notation
-    notation_layer_in = get_input_notation(layer, modules_details, prev_out_notation)
-    return notation_layer_out, notation_layer_in
+def get_layer_variable_input_not_reused(layer, prev_out_variable, modules_details):   
+    out_variable_layer = prev_out_variable
+    in_variable_layer = get_input_variable(layer, modules_details, prev_out_variable)
+    return out_variable_layer, in_variable_layer
 
-def get_tensorop_notation_input_reused(prev_out_notation):
-    if prev_out_notation == "x":
-        notation_ts_out = "x_1"
+def get_tensorop_out_var_input_reused(prev_out_variable):
+    if prev_out_variable == "x":
+        out_variable = "x_1"
     else:
-        notation_ts_out = f"x_{int(prev_out_notation.split('_')[-1])+1}"
-    return notation_ts_out
+        out_variable = f"x_{int(prev_out_variable.split('_')[-1])+1}"
+    return out_variable
 
 
-def get_rnn_output_notation(modules_details):
+def get_rnn_output_variable(modules_details):
     for module_def in modules_details.values():
         if len(module_def) == 4:
             if module_def[-1].__class__.mro()[1].__name__ == "RNN" and module_def[-1].return_hidden:
@@ -217,19 +218,19 @@ def get_rnn_output_notation(modules_details):
                 module_def[1] = module_def[1] + ", _"      
     return modules_details
 
-def get_layer_notation(layer: Layer, modules_details: Dict) -> Dict:
+def get_layer_in_out_variables(layer: Layer, modules_details: Dict) -> Dict:
     layer_synt, modules_details = get_layer_syntax(layer, modules_details)
     if (len(modules_details) == 0):
-        notation_layer_out, notation_layer_in = get_layer_notation_empty_dict(layer)
+        out_variable_layer, in_variable_layer = get_layer_variable_empty_dict(layer)
     else:
         previous_module = list(modules_details.keys())[-1]
-        prev_out_notation = get_previous_out_notation(modules_details, previous_module)
+        prev_out_variable = get_previous_out_variable(modules_details, previous_module)
         if layer.input_reused == True:
-            notation_layer_out, notation_layer_in = get_layer_notation_input_reused(layer, prev_out_notation, modules_details)
+            out_variable_layer, in_variable_layer = get_layer_variable_input_reused(layer, prev_out_variable, modules_details)
         else:
-            notation_layer_out, notation_layer_in = get_layer_notation_input_not_reused(layer, prev_out_notation, modules_details)
+            out_variable_layer, in_variable_layer = get_layer_variable_input_not_reused(layer, prev_out_variable, modules_details)
     
-    modules_details[layer.name + "_layer"] = [layer_synt, notation_layer_out, notation_layer_in, layer]
+    modules_details[layer.name + "_layer"] = [layer_synt, out_variable_layer, in_variable_layer, layer]
     return modules_details
 
 """if the output of a layer is expected to be reused as input to more than one layer,
@@ -238,26 +239,26 @@ the first layer that receives that output should have the parameter input_reused
 
 def get_tensorop_syntax(tensorOp, modules_details):
     previous_module = list(modules_details.keys())[-1]
-    prev_out_notation = get_previous_out_notation(modules_details, previous_module)
+    prev_out_variable = get_previous_out_variable(modules_details, previous_module)
     if tensorOp.type == "reshape":
         reshape_dim = ', '.join([str(i) for i in tensorOp.reshape_dim])
-        ts_op_synt = f"tf.reshape({prev_out_notation}, {reshape_dim})"
+        ts_op_synt = f"tf.reshape({prev_out_variable}, {reshape_dim})"
     elif tensorOp.type == "concatenate":
-        tensors = get_layers_notation_for_tensors(tensorOp.layers_of_tensors, modules_details)
+        tensors = get_layers_output_for_tensorops(tensorOp.layers_of_tensors, modules_details)
         tensors = ', '.join(tensors)
         ts_op_synt = f"tf.concat([{tensors}], axis={tensorOp.concatenate_dim})"
     elif tensorOp.type == "transpose":
         transpose_dim = ", ".join([str(i) for i in tensorOp.transpose_dim])
-        ts_op_synt = f"tf.transpose({prev_out_notation}, perm=[{transpose_dim}])"
+        ts_op_synt = f"tf.transpose({prev_out_variable}, perm=[{transpose_dim}])"
     elif tensorOp.type == "permute":
         permute_dim = ", ".join([str(i) for i in tensorOp.permute_dim])
-        ts_op_synt = f"tf.transpose({prev_out_notation}, perm=[{permute_dim}])"
+        ts_op_synt = f"tf.transpose({prev_out_variable}, perm=[{permute_dim}])"
     else:
         tensors = []
         for elem in tensorOp.layers_of_tensors:
             if type(elem) == str:
-                notation_out_layer = get_layers_notation_for_tensors([elem], modules_details)[0]
-                tensors.append(notation_out_layer)
+                out_variable_layer = get_layers_output_for_tensorops([elem], modules_details)[0]
+                tensors.append(out_variable_layer)
             else:
                 tensors.append(elem)
         tensors = ', '.join([str(i) for i in tensors])
@@ -268,19 +269,19 @@ def get_tensorop_syntax(tensorOp, modules_details):
             ts_op_synt = f"tf.matmul({tensors})"
     return ts_op_synt
 
-def get_tensorop_notation(tensorOp: TensorOp, modules_details: Dict) -> Dict:
+def get_tensorop_out_variable(tensorOp: TensorOp, modules_details: Dict) -> Dict:
     ts_op_synt = get_tensorop_syntax(tensorOp, modules_details)
     if len(modules_details) == 0:
-        notation_ts_out  = get_tensorop_notation_empty_dict(tensorOp)
+        out_variable  = get_tensorop_output_empty_dict(tensorOp)
     else:
         previous_module = list(modules_details.keys())[-1]
-        prev_out_notation = get_previous_out_notation(modules_details, previous_module)
+        prev_out_variable = get_previous_out_variable(modules_details, previous_module)
         if tensorOp.input_reused == True:
-            notation_ts_out  = get_tensorop_notation_input_reused(prev_out_notation)
+            out_variable  = get_tensorop_out_var_input_reused(prev_out_variable)
         else:   
-            notation_ts_out = prev_out_notation
+            out_variable = prev_out_variable
     
-    modules_details[tensorOp.name + "_op"] = [ts_op_synt, notation_ts_out]
+    modules_details[tensorOp.name + "_op"] = [ts_op_synt, out_variable]
     return modules_details
 
 

f748fd8b29031afe62dd093a2d167b00b29813cd
ivan-alfonso <ivandaalf@hotmail.com>
2024-11-06 10:00:36 +0100

    Add generation of the file with the base code to build the B-UML model

--- besser/BUML/notations/structuralPlantUML/plantuml_to_buml.py
+++ besser/BUML/notations/structuralPlantUML/plantuml_to_buml.py
@@ -1,16 +1,17 @@
 from antlr4 import CommonTokenStream, FileStream, ParseTreeWalker
+from besser.BUML.notations.structuralDrawioXML import save_buml_to_file
 from besser.BUML.metamodel.structural import DomainModel
 from .PlantUMLLexer import PlantUMLLexer
 from .PlantUMLParser import PlantUMLParser
 from .plantUML_buml_listener import BUMLGenerationListener
 
-def plantuml_to_buml(plantUML_model_path:str, buml_model_file_name:str = "buml_model"):
+def plantuml_to_buml(plantUML_model_path:str, buml_file_path:str = None):
     """Transforms a PlantUML model into a B-UML model.
 
     Args:
         plantUML_model_path (str): The path to the file containing the PlantUML code.
-        buml_model_file_name (str, optional): the name of the file produced with the base 
-                code to build the B-UML model.
+        buml_file_path (str, optional): the path of the file produced with the base 
+                code to build the B-UML model (None as default).
 
     Returns:
         BUML_model (DomainModel): the B-UML model object.
@@ -22,4 +23,6 @@ def plantuml_to_buml(plantUML_model_path:str, buml_model_file_name:str = "buml_m
     walker = ParseTreeWalker()
     walker.walk(listen, parse_tree)
     domain_model: DomainModel = listen.get_buml_model()
+    if buml_file_path is not None:
+        save_buml_to_file(model=domain_model, file_name=buml_file_path)
     return domain_model

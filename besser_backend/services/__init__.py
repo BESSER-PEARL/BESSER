84ffa13234cf89ee8cbb415ac81f2a30775ffb10
ArmSL <scorp-arm@hotmail.com>
2024-11-25 16:13:53 +0100

    apollon api backend

---
+++ besser_backend/services/__init__.py
@@ -0,0 +1,4 @@
+from .buml_to_json import parse_buml_content, domain_model_to_json
+from .json_to_buml import json_to_buml
+
+__all__ = ['parse_buml_content', 'domain_model_to_json', 'json_to_buml']

77b36cbc474d8f3cac92619ddae53231712c16d1
Fitash UL-HAQ <fitash.ulhaq@list.lu>
2025-01-28 16:12:35 +0100

    update parser

--- besser/BUML/notations/ocl/OCLParserWrapper.py
+++ besser/BUML/notations/ocl/OCLParserWrapper.py
@@ -12,12 +12,15 @@ class OCLParserWrapper:
     def parse(self, ocl):
         input_stream = InputStream(ocl.expression)
         rootHandler = Root_Handler(self.dm, self.om)
+        rootHandler.set_context(ocl.context)
         lexer = BOCLLexer(input_stream)
         stream = CommonTokenStream(lexer)
         parser = BOCLParser(stream)
         tree = parser.oclFile()
         listener = BOCLListener(rootHandler)
+        listener.preprocess(ocl.expression)
         walker = ParseTreeWalker()
         walker.walk(listener, tree)
+
 
         return True

42c5ed3d86cd5427343fa7361d40fbd4b7452d83
Aran30 <aaron30@live.be>
2025-05-16 16:30:50 +0200

    added llm to agent metamodel

--- besser/BUML/metamodel/state_machine/agent.py
+++ besser/BUML/metamodel/state_machine/agent.py
@@ -148,6 +148,266 @@ class LLMIntentClassifierConfiguration(IntentClassifierConfiguration):
         self.use_entity_synonyms: bool = use_entity_synonyms
 
 
+class LLMWrapper(ABC):
+    """The LLM Wrapper class.
+
+    Args:
+        name (str): the LLM name
+        parameters (dict): the LLM parameters (this will vary depending on the LLM)
+        global_context (str): the global context to be used in the LLM prompt
+
+    Attributes:
+        name (str): the LLM name
+        parameters (dict): the LLM parameters (this will vary depending on the LLM)
+        global_context (str): the global context to be used in the LLM prompt
+    """
+
+    def __init__(
+            self,
+            name: str,
+            agent: 'Agent',
+            parameters: dict,
+            global_context: str = None
+    ):
+        self.name: str = name
+        self.parameters: dict = parameters
+        self.global_context: str = global_context
+        self._user_context: dict = dict()
+        self._user_contexts: dict = dict()
+
+        agent.llms.append(self)
+
+    def predict(self, message: str, parameters: dict = None,
+                system_message: str = None) -> str:
+        """Make a prediction, i.e., generate an output.
+
+        Args:
+            message (Any): the LLM input text
+            session (Session): the ongoing session, can be None if no context needs to be applied
+            parameters (dict): the LLM parameters to use in the prediction. If none is provided, the default LLM
+                parameters will be used
+            system_message (str): system message to give high priority context to the LLM
+
+        Returns:
+            str: the LLM output
+        """
+        return None
+
+    def chat(self, parameters: dict = None, system_message: str = None) -> str:
+        """Make a prediction, i.e., generate an output.
+
+        This function can provide the chat history to the LLM for the output generation, simulating a conversation or
+        remembering previous messages.
+
+        Args:
+            session (Session): the user session
+            parameters (dict): the LLM parameters. If none is provided, the RAG's default value will be used
+            system_message (str): system message to give high priority context to the LLM
+       
+        Returns:
+            str: the LLM output
+        """
+        return None
+
+    def add_user_context(self, context: str, context_name: str) -> None:
+        """Add user-specific context.
+
+        Args:
+            session (Session): the ongoing session
+            context (str): the user-specific context
+            context_name (str): the key given to the specific user context
+        """
+
+    def remove_user_context(self, context_name: str) -> None:
+        """Remove user-specific context.
+
+        Args:
+            session (Session): the ongoing session
+            context_name (str): the key given to the specific user context
+        """
+
+
+class LLMHuggingFace(LLMWrapper):
+    """A HuggingFace LLM wrapper.
+
+    Normally, we consider an LLM in HuggingFace those models under the tasks ``text-generation`` or
+    ``text2text-generation`` tasks (`more info <https://huggingface.co/tasks/text-generation>`_), but there could be
+    exceptions for other tasks (which have not been tested in this class).
+
+    Args:
+        agent (Agent): the agent the LLM belongs to
+        name (str): the LLM name
+        parameters (dict): the LLM parameters
+        num_previous_messages (int): for the chat functionality, the number of previous messages of the conversation
+            to add to the prompt context (must be > 0).
+        global_context (str): the global context to be provided to the LLM for each request
+
+
+    Attributes:
+        name (str): the LLM name
+        parameters (dict): the LLM parameters
+        num_previous_messages (int): for the chat functionality, the number of previous messages of the conversation
+            to add to the prompt context (must be > 0).
+        _global_context (str): the global context to be provided to the LLM for each request
+    """
+
+    def __init__(self, agent: 'Agent', name: str, parameters: dict, num_previous_messages: int = 1,
+                 global_context: str = None):
+        super().__init__(name, agent, parameters, global_context=global_context)
+        self.agent: 'Agent' = agent
+        self.num_previous_messages: int = num_previous_messages
+
+    def set_model(self, name: str) -> None:
+        """Set the LLM model name.
+
+        Args:
+            name (str): the new LLM name
+        """
+        self.name = name
+
+    def set_num_previous_messages(self, num_previous_messages: int) -> None:
+        """Set the number of previous messages to use in the chat functionality
+
+        Args:
+            num_previous_messages (int): the new number of previous messages
+        """
+        self.num_previous_messages = num_previous_messages
+
+
+class LLMHuggingFaceAPI(LLMWrapper):
+    """A HuggingFace LLM wrapper for HuggingFace's Inference API.
+
+    Normally, we consider an LLM in HuggingFace those models under the tasks ``text-generation`` or
+    ``text2text-generation`` tasks (`more info <https://huggingface.co/tasks/text-generation>`_), but there could be
+    exceptions for other tasks (which have not been tested in this class).
+
+    Args:
+        agent (Agent): the agent the LLM belongs to
+        name (str): the LLM name
+        parameters (dict): the LLM parameters
+        num_previous_messages (int): for the chat functionality, the number of previous messages of the conversation
+            to add to the prompt context (must be > 0).
+        global_context (str): the global context to be provided to the LLM for each request
+
+
+    Attributes:
+        name (str): the LLM name
+        parameters (dict): the LLM parameters
+        num_previous_messages (int): for the chat functionality, the number of previous messages of the conversation
+            to add to the prompt context (must be > 0).
+        _global_context (str): the global context to be provided to the LLM for each request
+    """
+
+    def __init__(self, agent: 'Agent', name: str, parameters: dict, num_previous_messages: int = 1,
+                 global_context: str = None):
+        super().__init__(name, agent, parameters, global_context=global_context)
+        self.agent: 'Agent' = agent
+        self.num_previous_messages: int = num_previous_messages
+
+    def set_model(self, name: str) -> None:
+        """Set the LLM model name.
+
+        Args:
+            name (str): the new LLM name
+        """
+        self.name = name
+
+    def set_num_previous_messages(self, num_previous_messages: int) -> None:
+        """Set the number of previous messages to use in the chat functionality
+
+        Args:
+            num_previous_messages (int): the new number of previous messages
+        """
+        self.num_previous_messages = num_previous_messages
+
+
+class LLMOpenAI(LLMWrapper):
+    """An LLM wrapper for OpenAI's LLMs through its API.
+
+    Args:
+        agent (Agent): the agent the LLM belongs to
+        name (str): the LLM name
+        parameters (dict): the LLM parameters
+        num_previous_messages (int): for the chat functionality, the number of previous messages of the conversation
+            to add to the prompt context (must be > 0).
+        global_context (str): the global context to be provided to the LLM for each request
+
+    Attributes:
+        _nlp_engine (NLPEngine): the NLPEngine that handles the NLP processes of the agent the LLM belongs to
+        name (str): the LLM name
+        parameters (dict): the LLM parameters
+        num_previous_messages (int): for the chat functionality, the number of previous messages of the conversation
+            to add to the prompt context (must be > 0).
+        _global_context (str): the global context to be provided to the LLM for each request
+        _user_context (dict): user specific context to be provided to the LLM for each request
+    """
+
+    def __init__(self, agent: 'Agent', name: str, parameters: dict, num_previous_messages: int = 1,
+                 global_context: str = None):
+        super().__init__(name, agent, parameters, global_context=global_context)
+        self.agent: 'Agent' = agent
+        self.num_previous_messages: int = num_previous_messages
+
+    def set_model(self, name: str) -> None:
+        """Set the LLM model name.
+
+        Args:
+            name (str): the new LLM name
+        """
+        self.name = name
+
+    def set_num_previous_messages(self, num_previous_messages: int) -> None:
+        """Set the number of previous messages to use in the chat functionality
+
+        Args:
+            num_previous_messages (int): the new number of previous messages
+        """
+        self.num_previous_messages = num_previous_messages
+
+
+class LLMReplicate(LLMWrapper):
+    """An LLM wrapper for Replicate's LLMs through its API.
+
+    Args:
+        agent (Agent): the agent the LLM belongs to
+        name (str): the LLM name
+        parameters (dict): the LLM parameters
+        num_previous_messages (int): for the chat functionality, the number of previous messages of the conversation
+            to add to the prompt context (must be > 0)
+        global_context (str): the global context to be provided to the LLM for each request
+
+    Attributes:
+        _nlp_engine (NLPEngine): the NLPEngine that handles the NLP processes of the agent the LLM belongs to
+        name (str): the LLM name
+        parameters (dict): the LLM parameters
+        num_previous_messages (int): for the chat functionality, the number of previous messages of the conversation
+            to add to the prompt context (must be > 0)
+        _global_context (str): the global context to be provided to the LLM for each request
+        _user_context (dict): user specific context to be provided to the LLM for each request
+    """
+
+    def __init__(self, agent: 'Agent', name: str, parameters: dict, num_previous_messages: int = 1,
+                 global_context: str = None):
+        super().__init__(name, agent, parameters, global_context=global_context)
+        self.agent: 'Agent' = agent
+        self.num_previous_messages: int = num_previous_messages
+
+    def set_model(self, name: str) -> None:
+        """Set the LLM model name.
+
+        Args:
+            name (str): the new LLM name
+        """
+        self.name = name
+
+    def set_num_previous_messages(self, num_previous_messages: int) -> None:
+        """Set the number of previous messages to use in the chat functionality
+
+        Args:
+            num_previous_messages (int): the new number of previous messages
+        """
+        self.num_previous_messages = num_previous_messages
+
 class AgentSession(Session):
     """A user session in a agent execution.
 
@@ -803,6 +1063,7 @@ class Agent(StateMachine):
         self.intents: list[Intent] = []
         self.entities: list[Entity] = []
         self.global_initial_states: list[tuple[AgentState, Intent]] = []
+        self.llms: list[LLMWrapper] = []
 
     def new_state(self,
                   name: str,
